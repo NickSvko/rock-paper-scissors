fd978d3c66cecc8e53abddd6eb2b15fda77df9f8
Nick Savchenko <NickSvko@gmail.com>
2023-12-03 18:18:32 +0200

    Add roundResult() and capitalize() functions

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ function getComputerChoice() {
     choiceIndex = Math.floor(Math.random() * choices.length);
     return choices[choiceIndex];
 }
+
+
+function roundResult(state, playerSelection, computerSelection) {
+    let winner, looser;
+
+    if(state == "Win") {
+        winner = playerSelection;
+        looser = computerSelection;
+    }
+    else {
+        winner = computerSelection;
+        looser = playerSelection;
+    }
+    return `You ${state}! ${winner} beats ${looser}`;
+}
+
+
+function capitalizeWord(word) {
+    firstChar = word.charAt(0);
+    firstChar = firstChar.toUpperCase();
+    restOfTheWord = word.slice(1).toLowerCase();
+    return firstChar + restOfTheWord;
+}
