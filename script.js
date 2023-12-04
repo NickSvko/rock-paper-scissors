696b897024a9c5e223737444f7258334d96a1db1
Nick Savchenko <NickSvko@gmail.com>
2023-12-04 12:03:16 +0200

    Add game() function

--- script.js
+++ script.js
@@ -35,16 +35,18 @@ function playRound(playerSelection, computerSelection) {
 
 function resultMassage(roundResult, playerSelection, computerSelection) {
     let winnerSignal, loserSignal;
+    let playerChoice = capitalizeWord(playerSelection);
+
     if(roundResult === "tie") {
         return "It's a TIE!"
     }
     if(roundResult === "Player") {
-        winnerSignal = playerSelection;
+        winnerSignal = playerChoice;
         loserSignal = computerSelection;
     }
     else {
         winnerSignal = computerSelection;
-        loserSignal = playerSelection;
+        loserSignal = playerChoice;
     }
     return `${roundResult} Wins! ${winnerSignal} beats ${loserSignal}`;
 }
@@ -55,4 +57,38 @@ function capitalizeWord(word) {
     firstChar = firstChar.toUpperCase();
     let restOfTheWord = word.slice(1).toLowerCase();
     return firstChar + restOfTheWord;
+}
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    let winner;
+
+    for(let roundNumber = 1; roundNumber <= 5; roundNumber++) {
+        let playerSelection = prompt("Insert your choice: ");
+        let computerSelection = getComputerChoice();
+        let roundWinner = playRound(playerSelection, computerSelection);
+        
+        if(roundWinner === 'Computer') {
+            computerWins++;
+        }
+        else if(roundWinner === 'Player') {
+            playerWins++;
+        }
+
+        let result = resultMassage(roundWinner, playerSelection, computerSelection);
+        console.log(`round ${roundNumber}: ${result}`);
+    }
+    if(playerWins > computerWins) {
+        winner = "Player wins!";
+    }
+    else if(playerWins < computerWins) {
+        winner = "Computer wins!";
+    }
+    else {
+        winner = "It's a TIE!"
+    }
+    console.log(`Final Score:
+    Player wins ${playerWins} times, computer wins ${computerWins} times.
+    The match result: ${winner}`)
 }
